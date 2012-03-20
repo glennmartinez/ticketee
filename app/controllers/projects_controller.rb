091024c71ba4579337e547f8dca4796f7c3959a9
glennpc <glennpc@glennpc.(none)>
2012-03-20 23:46:11 +1100

    users should be redirected back to projects page if use invalid url

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,9 @@
 class ProjectsController < ApplicationController
+	before_filter :find_project, :only => [:show,
+										   :edit,
+										   :update,
+										   :destroy]
+
 
 	def index
 		@projects = Project.all
@@ -26,16 +31,16 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-		@project = Project.find(params[:id])
+		# @project = Project.find(params[:id]) before filter actions the project.find
 
 	end
 
 	def edit
-		@project = Project.find(params[:id])
+		# @project = Project.find(params[:id])
 	end
 
 	def update
-		@project = Project.find(params[:id])
+		# @project = Project.find(params[:id])
 		if @project.update_attributes(params[:project])
 				flash[:notice] = "Project has been updated."
 				redirect_to @project
@@ -46,11 +51,21 @@ class ProjectsController < ApplicationController
 	end
 
 	def destroy
-		@project = Project.find(params[:id])
+		# @project = Project.find(params[:id])
 		@project.destroy
 		flash[:notice] = "Project has been deleted."
 		redirect_to projects_path
 	end
+
+	private
+		def find_project
+			@project = Project.find(params[:id])
+			rescue ActiveRecord::RecordNotFound
+			flash[:alert] = "The project you were looking" +
+							 " for could not be found."
+			redirect_to projects_path
+			
+		end
 
 
 end
