79f4c076b3e42e8cefe6e44a033f36ae81560d76
glenn martin <glennmartin76@glenns-MacBook-Air-2.local>
2012-03-22 22:39:59 +1100

    added ticket deletion

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -41,6 +41,13 @@ class TicketsController < ApplicationController
 		
 	end
 
+	def destroy
+		@ticket.destroy
+		flash[:notice] = "Ticket has been deleted."
+		redirect_to @project
+		
+	end
+
 
 	def find_project
 		@project = Project.find(params[:project_id])
